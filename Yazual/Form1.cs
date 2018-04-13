dc29130a59afe27baaa20003877c266df4a333fd
dach <dan4i4ek.ch@gmail.com>
2018-04-13 13:01:14 +0300

    111

---
+++ Yazual/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazual
+{
+    
+    public partial class Form1 : Form
+    {
+        int S;
+        int k;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          if(textBox1.Text!=String.Empty && textBox2.Text != String.Empty)
+            {
+                S = Convert.ToInt32(textBox1.Text);
+                k = Convert.ToInt32(textBox2.Text);
+
+                // this.ShowInTaskbar = false;
+                //this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+                this.Visible = false;
+                Form2 f = new Form2(this.S, this.k);
+                f.ShowDialog();
+            } else { MessageBox.Show("Empty one or two boxes. Please, type something...","Error"); }
+            
+         
+        }
+    }
+}
---
+++ Yazual/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazual
+{
+    public partial class Form2 : Form
+    {
+        int S, k;  
+        public Form2(int S, int k)
+        {
+            InitializeComponent();
+            this.S = S;
+            this.k = k;
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           for (int i = 0; i < k; i++)
+            {
+                this.Controls.Add(new TextBox() { Name = i.ToString(), Location = new Point(152 + i * 150, 3), Text = "" });
+                for(int j = 0; j < S; j++)
+                {
+                    this.Controls.Add(new TextBox() { Name = i.ToString(), Location = new Point(2, 40 + j * 40), Text = "" });
+                    this.Controls.Add(new TextBox() { Name = i.ToString(), Location = new Point(152 + i * 150, 40 + j * 40), Text = "" });
+                }
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
